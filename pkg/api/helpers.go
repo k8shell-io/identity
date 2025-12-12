0813d0c645c2d1227be239d30deec55f5d5578d6
bruckins <karelnebojsa@seznam.cz>
2025-12-12 11:23:51 +0100

    client close

--- pkg/api/helpers.go
+++ pkg/api/helpers.go
@@ -7,6 +7,7 @@ import (
 
 type Client struct {
 	identitypb.IdentityServiceClient
+	client *gapi.Client
 }
 
 func NewClient(cfg gapi.ClientConfig) (*Client, error) {
@@ -16,5 +17,10 @@ func NewClient(cfg gapi.ClientConfig) (*Client, error) {
 	}
 	return &Client{
 		IdentityServiceClient: identitypb.NewIdentityServiceClient(gapiClient.Conn),
+		client:                gapiClient,
 	}, nil
+}
+
+func (c *Client) Close() error {
+	return c.client.Close()
 }
